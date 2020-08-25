cbf4c51f9501648e31638f36db31ea1810818135
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-08-25 17:18:39 -0600

    Create User test

--- test/users-test.js
+++ test/users-test.js
@@ -3,3 +3,12 @@ const expect = chai.expect;
 
 const userData = require('../data/users-sub');
 const Users = require('../src/users');
+
+describe('Users', () => {
+  it('Should return a user\'s first name', () => {
+    let user = new Users(userData[0])
+    let userName = user.getFirstName();
+
+    expect(userName).to.equal('Luisa');
+  });
+});

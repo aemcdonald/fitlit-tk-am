c8f137a01f6ff4a1f1c9437cd968d4abe6511b6b
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-08-26 15:43:30 -0600

    Create test for get user & average fluid ounces function

--- test/hydration-test.js
+++ test/hydration-test.js
@@ -1,5 +1,23 @@
 const chai = require('chai');
 const expect = chai.expect;
+const moment = require('moment');
 
 const hydrationData = require('../data/hydration-sub');
 const Hydration = require('../src/hydration');
+
+describe('Hydration', () => {
+  let hydration;
+  beforeEach(() => {
+    hydration = new Hydration(hydrationData);
+  });
+
+  it('Should return single user based on ID', () => {
+    let user1 = hydration.getUser(1);
+    expect(user1[1]).to.equal(hydrationData[3]);
+  });
+
+  it('Should calculate a user\'s average fluid ounces for all time', () => {
+    let user1Avg = hydration.averageFluidOunces(1);
+    expect(user1Avg).to.equal(59.4);
+  });
+});

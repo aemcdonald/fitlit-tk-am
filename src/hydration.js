f4c8a60c44759a14b8427f459c5981bdd81b9998
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-08-26 15:44:05 -0600

    Create getUser & averageFluidOunces methods in Hydration class

--- src/hydration.js
+++ src/hydration.js
@@ -1,10 +1,27 @@
 const hydrationData = require('../data/hydration');
+const moment = require('moment');
 
 class Hydration {
-  constructor() {
+  constructor(hydrationData) {
+    this.hydrationData = hydrationData;
+  }
+  getUser(id) {
+    return this.hydrationData.filter(user => {
+      return user.userID === id;
+    });
+  }
+  averageFluidOunces(id) {
+    this.getUser(id);
+    let allOunces = this.hydrationData.reduce((totalOunces, user) => {
+      return totalOunces += user.numOunces
+    }, 0);
+    return allOunces / this.hydrationData.length;
+
 
   }
 }
+
+//    moment("2019/06/15").subtract(7, 'days').calendar();
 
 
 

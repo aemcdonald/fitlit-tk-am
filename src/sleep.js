fbd3f274aced6e72df761bfab16d4cbac336957a
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-08-27 16:06:22 -0600

    Create method to return user's hours slept over a given week

--- src/sleep.js
+++ src/sleep.js
@@ -37,8 +37,24 @@ class Sleep {
     });
     return currentUser.sleepQuality;
   }
+  dailySleepPerWeek(id, date) {
+    let userData = this.getUser(id);
+    let currentUser = userData.find(user => {
+      return user.date === date;
+    });
+    let index = userData.indexOf(currentUser);
+    let week = userData.slice(index - 6, index + 1);
+    return week.map(night => {
+      return night.hoursSlept;
+    });
+  }
+  // for a user (id parameter),  and needs a date parameter.
+  //run getUser to create an array of single user data.
+  // use indexOf to get var index of array element object that corresponds to date argument.
+  // run let week = userdata.slice(index - 6, index + 1).
+  // use map to return hoursSlept values.
+  // return will be an array of hoursSlept values.
 }
-
 
 
 if (typeof module !== 'undefined') {

41393f0e9615dc40f972acbe1e37e92320a7a4a1
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-08-27 13:44:51 -0600

    Create test to return user by ID

--- test/sleep-test.js
+++ test/sleep-test.js
@@ -3,3 +3,16 @@ const expect = chai.expect;
 
 const sleepData = require('../data/sleep-sub');
 const Sleep = require('../src/sleep');
+
+
+describe('Sleep', () => {
+  let sleep;
+  beforeEach(() => {
+    sleep = new Sleep(sleepData);
+  });
+
+  it('Should return single user based on ID', () => {
+    let user1 = sleep.getUser(1);
+    expect(user1[0]).to.equal(sleepData[0]);
+  });
+});

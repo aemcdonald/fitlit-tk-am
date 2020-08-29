5eaaa776c03b68e4862b0b9de75d361d8839807f
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-08-29 10:39:47 -0600

    Create method to return user miles walked

--- src/activity.js
+++ src/activity.js
@@ -1,10 +1,32 @@
  const activityData = require('../data/activity');
+ const userData = require('../data/users');
 
  class Activity {
-   constructor() {
-
+   constructor(activityData) {
+     this.activityData = activityData;
+     this.userData = userData;
    }
- }
+   getUser(id) {
+     return this.activityData.filter(user => {
+       return user.userID === id;
+     });
+  }
+  getObjectByDate(id, date) {
+    let userObj = this.getUser(id);
+    return userObj.find(user => {
+      return user.date === date;
+    });
+  }
+  getUserMilesWalked(id, date) {
+    let day = this.getObjectByDate(id, date);
+    let userInfo = this.userData.find(user => {
+      return user.id === id;
+    });
+    console.log(userInfo)
+    let totalFeet = userInfo.strideLength * day.numSteps;
+    return +parseFloat((totalFeet / 5280).toFixed(2));
+  }
+}
 
 
 

7eb4214900f66db4d24032a9d047aed1431d976e
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-09-02 15:19:32 -0600

    Refactor methods to one line

--- src/hydration.js
+++ src/hydration.js
@@ -1,26 +1,18 @@
-// const hydrationData = require('../data/hydration');
-
 class Hydration {
   constructor(hydrationData) {
     this.hydrationData = hydrationData;
   }
   getUser(id) {
-    return this.hydrationData.filter(user => {
-      return user.userID === id;
-    });
+    return this.hydrationData.filter(user => user.userID === id);
   }
   getMostRecentWeek() {
     let userData = this.getUser(1);
     let userWeekData = userData.slice(-7);
-    return userWeekData.map(day => {
-      return day.date;
-    });
+    return userWeekData.map(day => day.date);
   }
   getObjectByDate(id, date) {
     let userData = this.getUser(id);
-    return userData.find(user => {
-      return user.date === date;
-    });
+    return userData.find(user => user.date === date);
   }
   averageFluidOunces(id) {
     this.getUser(id);
@@ -36,9 +28,7 @@ class Hydration {
   fluidOuncesWeek(id) {
     let userData = this.getUser(id);
     let userWeekData = userData.slice(-7);
-    return userWeekData.map(day => {
-      return day.numOunces;
-    });
+    return userWeekData.map(day => day.numOunces);
   }
 }
 

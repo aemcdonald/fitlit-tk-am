9e186092e07011455fe18f593b7b514fe41d5597
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-09-02 15:51:26 -0600

    Clean up spacing

--- src/hydration.js
+++ src/hydration.js
@@ -2,18 +2,22 @@ class Hydration {
   constructor(hydrationData) {
     this.hydrationData = hydrationData;
   }
+
   getUser(id) {
     return this.hydrationData.filter(user => user.userID === id);
   }
+
   getMostRecentWeek() {
     let userData = this.getUser(1);
     let userWeekData = userData.slice(-7);
     return userWeekData.map(day => day.date);
   }
+
   getObjectByDate(id, date) {
     let userData = this.getUser(id);
     return userData.find(user => user.date === date);
   }
+
   averageFluidOunces(id) {
     this.getUser(id);
     let allOunces = this.hydrationData.reduce((totalOunces, user) => {
@@ -21,10 +25,12 @@ class Hydration {
     }, 0);
     return allOunces / this.hydrationData.length;
   }
+
   fluidOuncesOnDay(id, date) {
     let userOunces = this.getObjectByDate(id, date);
     return userOunces.numOunces;
   }
+  
   fluidOuncesWeek(id) {
     let userData = this.getUser(id);
     let userWeekData = userData.slice(-7);
